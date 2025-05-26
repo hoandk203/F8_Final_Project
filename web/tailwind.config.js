642ad3c7971e22e472a2bd8ef86091de04c756df
hoandk203 <hoanyttv@gmail.com>
2025-05-27 01:26:53 +0700

    tailwind confi: safelist. mot so MUI block tailwind (production)

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,6 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./src/**/*.{js,ts,jsx,tsx,mdx}',],
+    safelist: [
+        'bg-red-600',
+        'hover:bg-red-700',
+        'bg-yellow-50',
+        'border-yellow-200',
+        'text-yellow-700',
+        'text-gray-600',
+        'grid-cols-1',
+        'font-semibold',
+        'pt-4',
+        'pb-4',
+        'px-4',
+        'gap-y-2',
+        'py-2',
+        'px-4',
+        'rounded-lg',
+        'text-white',
+      ],
     theme: {
         extend: {},
         container: {
